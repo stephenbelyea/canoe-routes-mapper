efcd16df3fcfc8eb4b3d5031b573efca599e4de0
Stephen Belyea <belyea.stephen@gmail.com>
2026-03-25 23:44:23 -0400

    Pages config and deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "leaflet/dist/leaflet.css";
 import { Overview } from "./views";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Overview />
-  </StrictMode>
+  </StrictMode>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/canoe-routes-mapper/",
   plugins: [react()],
   optimizeDeps: {
     include: [
