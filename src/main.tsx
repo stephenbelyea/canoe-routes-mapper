e1e908351c1cd7ff616e2ad90bf07de3b4ffeaf1
Stephen Belyea <belyea.stephen@gmail.com>
2026-03-26 20:25:10 -0400

    Shift stylesheets into src/assets and import through react app, add .nojekyll file for cleaner processing

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "leaflet/dist/leaflet.css";
 import { Overview } from "./views";
+
+import "./assets/reset.css";
+import "./assets/styles.css";
+import "leaflet/dist/leaflet.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
